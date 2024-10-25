0df5cf35ce9cd7ed7b6a76880506f215cdf12365
LeonardPM <leonardpaccham@gmail.com>
2024-10-24 20:48:56 -0500

    dbr

--- PoyectoTaller3/src/Ejercicio02.java
+++ PoyectoTaller3/src/Ejercicio02.java
@@ -1,2 +1,11 @@
 public class Ejercicio02 {
+    public static void main(String[] args) {
+        int v1, v2, v3;
+        double respuesta;
+        v1 = 10;
+        v2 = 2;
+        v3 = 10;
+        respuesta = Math.pow(v1,v2)+v3;
+        System.out.println("respuesta = " + respuesta);
+    }
 }
