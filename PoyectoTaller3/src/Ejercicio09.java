9d119f6b1ad86a9191da75d7d0c09e58837a7c8b
LeonardPM <leonardpaccham@gmail.com>
2024-10-26 20:08:21 -0500

    eje-9-10

--- PoyectoTaller3/src/Ejercicio09.java
+++ PoyectoTaller3/src/Ejercicio09.java
@@ -1,2 +1,16 @@
 public class Ejercicio09 {
+    public static void main(String[] args) {
+        int v1, v2, v3, v4;
+        double operacion1;
+        boolean respuesta;
+        v1 = 81;
+        v2 = 25;
+        v3 = 10;
+        v4 = 50;
+
+        operacion1 = Math.sqrt(v1) + Math.sqrt(v2) * v3;
+        respuesta = operacion1 > v4;
+        System.out.println("--------- Solución del problema ------------------");
+        System.out.println("Respuesta = " + respuesta);
+    }
 }
--- PoyectoTaller3/src/Ejercicio10.java
+++ PoyectoTaller3/src/Ejercicio10.java
@@ -1,2 +1,23 @@
 public class Ejercicio10 {
+    public static void main(String[] args) {
+        int v1, v2, v3, v6, v7, v8;
+        double operacion01, operacion02;
+        boolean respuesta, v4, v5;
+        v1 = 25;
+        v2 = 10;
+        v3 = 100;
+        v4 = true;
+        v5 = false;
+        v6 = 10;
+        v7 = 5;
+        v8 = 2;
+
+        operacion01 = Math.sqrt(v1) * v2;
+        operacion02 = v6 / v7;
+        respuesta = operacion01 >= v3 && v4 || v5 || operacion02 >= v8;
+
+        System.out.println("--------- Solución del problema ------------------");
+        System.out.println("Respuesta = " + respuesta);
+
+    }
 }
